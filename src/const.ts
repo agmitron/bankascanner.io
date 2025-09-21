6659b0eb94a39a63a167b8b89a665a05ab679055
Aleksei Gmitron <me@agmitron.dev>
2025-09-21 20:01:19 +0400

    +

--- src/const.ts
+++ src/const.ts
@@ -3,14 +3,14 @@ import * as exporter from "bankascanner/exporter";
 
 type Loader<D> = () => Promise<D>;
 
-export const importers: Loader<importer.Definition>[] = [
-	load("https://agmitron.github.io/bankascanner/tbank/tbank.js"),
-	load("https://agmitron.github.io/bankascanner/tbc/tbc.js"),
-];
+export const importers: Record<string, Loader<importer.Definition>> = {
+	tbank: load("https://agmitron.github.io/bankascanner/tbank/tbank.js"),
+	tbc: load("https://agmitron.github.io/bankascanner/tbc/tbc.js"),
+};
 
-export const exporters: Loader<exporter.Definition>[] = [
-	load("bankascanner/exporter/json"),
-];
+export const exporters: Record<string, Loader<exporter.Definition>> = {
+	json: load("bankascanner/exporter/json"),
+};
 
 function load<D>(url: string): Loader<D> {
 	return () => import(url);
